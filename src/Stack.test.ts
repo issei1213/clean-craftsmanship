6609562e7977a6d8468a77c4fb927904f07673bc
issei1213 <59830008+issei1213@users.noreply.github.com>
2023-07-27 01:18:29 +0900

    [update]　StackのTDD実装練習

--- src/Stack.test.ts
+++ src/Stack.test.ts
@@ -1,7 +1,71 @@
-import { Stack } from './Stack';
+import { Stack, Underflow } from './Stack';
 
 test('canCreateStack', () => {
     const stack = new Stack();
 
-    expect(stack.isEmpty()).toEqual(true);
+    expect(stack.isEmpty()).toBe(true);
+});
+
+test('afterOnePush_isNotEmpty', () => {
+    const stack = new Stack();
+    stack.push(0);
+
+    expect(stack.isEmpty()).toBe(false);
+});
+
+test('afterOnePushAndOnePop_isEmpty', () => {
+    const stack = new Stack();
+
+    stack.push(0);
+    stack.pop();
+    expect(stack.isEmpty()).toBe(true);
+});
+
+test('afterTwoPushes_sizeIsTwo', () => {
+    const stack = new Stack();
+
+    stack.push(0);
+    stack.push(0);
+    expect(stack.getSize()).toBe(2);
+});
+
+test('afterOnePushAndOnePop_isEmpty', () => {
+    const stack = new Stack();
+
+    stack.push(0);
+    stack.pop();
+    expect(stack.isEmpty()).toBe(true);
+    expect(stack.getSize()).toBe(0);
+});
+
+test('afterOnePush_isNotEmpty', () => {
+    const stack = new Stack();
+
+    stack.push(0);
+    expect(stack.isEmpty()).toBe(false);
+    expect(stack.getSize()).toBe(1);
+});
+
+test('poppingEmptyStack_throwsUnderflow', () => {
+    const stack = new Stack();
+
+    expect(() => stack.pop()).toThrow(Underflow);
+});
+
+test('afterPushingX_willPopX', () => {
+    const stack = new Stack();
+
+    stack.push(99);
+    expect(stack.pop()).toBe(99);
+    stack.push(88);
+    expect(stack.pop()).toBe(88);
+});
+
+test('afterPushingXAndY_willPopYthenX', () => {
+    const stack = new Stack();
+
+    stack.push(99);
+    stack.push(88);
+    expect(stack.pop()).toBe(88);
+    expect(stack.pop()).toBe(99);
 });
--- src/Stack.ts
+++ src/Stack.ts
@@ -1,5 +1,26 @@
+export class Underflow extends Error {}
 export class Stack {
+    private empty: boolean = true;
+    private size: number = 0;
+    private elements: number[] = [];
+
     isEmpty() {
-        return true;
+        return this.size === 0;
+    }
+
+    push(element: number) {
+        this.size++;
+        this.elements[this.size] = element;
+    }
+
+    pop() {
+        if (this.size === 0) throw new Underflow();
+
+        this.size--;
+        return this.elements[this.size + 1];
+    }
+
+    getSize(): number {
+        return this.size;
     }
 }
